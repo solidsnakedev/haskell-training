cf92819356727fc60476853f0e5003296e02c137
Jonathan Rodriguez <jonathan@Jonathans-MacBook-Pro.local>
2022-03-15 06:16:30 -0400

    first release

---
+++ IOSession.hs
@@ -0,0 +1,40 @@
+import System.IO (hFlush, stdout)
+
+flush = hFlush stdout
+
+getFullName :: IO Person
+getFullName = 
+    putStrLn "Enter firstname: " >>
+    getLine   >>= \fn -> 
+    putStrLn "Enter lastname: " >>
+    readLn  >>= \ln ->
+    putStrLn "Enter Age: " >>
+    readLn  >>= \age' ->
+    putStrLn "Enter Color: " >>
+    readLn  >>= \color' ->
+    pure (Person fn ln age' color')
+
+--helper :: Read a => String -> IO a
+helper msg = putStrLn msg >> readLn 
+
+data Color = Red | Green | Blue
+    deriving (Show, Read)
+
+data Person = Person {
+    fname :: String,
+    lname :: String,
+    age   :: Int,
+    color :: Color
+}
+    deriving Read
+
+instance Show Person where
+  show p = "the first name is " ++ show (fname p) ++
+                                        "\nthe lastname name is " ++ show (lname p) ++
+                                        "\nage is " ++ show (age p) ++
+                                        "\nfav color is " ++ show (color p) ++ " "
+
+--main = getFullName >>= \n -> putStr (show n)
+main = getFullName >>= putStr . show
+
+--export PATH=/opt/homebrew/opt/llvm/bin/:$PATH
