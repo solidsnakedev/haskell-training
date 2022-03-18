80d73db74542af41da4ed63dc0d1e444d4e0293f
SolidSnake <solid@snake>
2022-03-18 11:50:41 -0400

    fixed isPrime , and adding gcd function

--- 99-questions/Questions31-41.hs
+++ 99-questions/Questions31-41.hs
@@ -1,9 +1,20 @@
 
 
-isPrime x = checkPrime x x
+isPrime x = checkPrime x (x-1)
     where
-        checkPrime x 0 = False
-        checkPrime x 1 = False
-        checkPrime x n 
-            | x `mod` (n-1) /= 0 = checkPrime x (n-1)
-            | otherwise = True
+        checkPrime x 0 = True
+        checkPrime x 1 = True
+        checkPrime x n
+            | (x `mod` n) == 0 = False
+            | otherwise = checkPrime x (n-1)
+
+
+-- (gcd 36 63)
+-- 9
+
+gcd x y =  getGCD (abs x) (abs y) (min (abs x) (abs y))
+    where
+        getGCD x y 0 = 0
+        getGCD x y minN
+            | (x `mod` minN) == 0 && (y `mod` minN) == 0 = minN
+            | otherwise = getGCD x y (minN - 1)
